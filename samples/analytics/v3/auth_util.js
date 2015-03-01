f442ab56f24bf58a9ad725c6c7edb2813d0d6c5e
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2015-03-01 02:05:30 -0800

    Update auth_util.js

--- samples/analytics/v3/auth_util.js
+++ samples/analytics/v3/auth_util.js
@@ -100,7 +100,7 @@ function handleUnauthorized() {
   var authorizeButton = document.getElementById('authorize-button');
   var runDemoButton = document.getElementById('run-demo-button');
 
-  runDemoButton.style.visibility = 'hidden';
+  runDemoButton.style.visibility = '';
   authorizeButton.style.visibility = '';
   authorizeButton.onclick = handleAuthClick;
   outputToPage('Please authorize this script to access Google Analytics.');
