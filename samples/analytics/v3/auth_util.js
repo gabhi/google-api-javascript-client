42709787ea1ece17cc635bc79e5dad0e2851d103
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2015-03-01 01:27:10 -0800

    Update auth_util.js

--- samples/analytics/v3/auth_util.js
+++ samples/analytics/v3/auth_util.js
@@ -28,8 +28,8 @@
  * section in the Google JavaScript Client Library.
  * https://code.google.com/p/google-api-javascript-client/wiki/Authentication
  */
-var clientId = '821751250764.apps.googleusercontent.com';
-var apiKey = 'AIzaSyAPusS7gzp0bTla1ogGW_hJOwamaBwVT5Q';
+var clientId = '893691437207-lncm7of2ql8kfhmphmm83eme6notp2gt.apps.googleusercontent.com';//'821751250764.apps.googleusercontent.com';
+var apiKey = '5QdBe9lpMpkdRuYPMKiXUUJc';
 var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
 
 
